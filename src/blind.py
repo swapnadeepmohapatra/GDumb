f80730436910ad6ece7ed27595f46b598ab47af4
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2023-12-02 23:16:14 +0530

    [Fixed] transform

--- src/blind.py
+++ src/blind.py
@@ -5,17 +5,17 @@ from unlearn import blindspot_unlearner
 
 from main import *
 
-transform_train = torchvision.transforms.Compose([
-	torchvision.transforms.Resize(224),
-	torchvision.transforms.ToTensor(),
-	torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
 # transform_train = torchvision.transforms.Compose([
-# 	torchvision.transforms.RandomCrop(28, padding=4),
-# 	torchvision.transforms.ToTensor(), # first, convert image to PyTorch tensor
-# 	torchvision.transforms.Normalize((0.1307,),(0.3081,)) # normalize inputs
-# ]), 
+# 	torchvision.transforms.Resize(224),
+# 	torchvision.transforms.ToTensor(),
+# 	torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+# ])
+
+transform_train = torchvision.transforms.Compose([
+	torchvision.transforms.RandomCrop(224),
+	torchvision.transforms.ToTensor(), # first, convert image to PyTorch tensor
+	torchvision.transforms.Normalize((0.1307),(0.3081)) # normalize inputs
+]), 
 
 
 if __name__ == '__main__':
